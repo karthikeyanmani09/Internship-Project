d6f839922ac21b3e16d7bc09cd9d99e7b63f6ebd
M.Karthi Keyan <118340083+karthikeyanmani09@users.noreply.github.com>
2022-12-28 15:52:41 +0530

    update

--- tests/unit/UpdateTodo.spec.js
+++ tests/unit/UpdateTodo.spec.js
@@ -1,5 +1,9 @@
 import {shallowMount} from "@vue/test-utils";
 import UpdateTodo from "@/pages/UpdateTodo.vue";
+import moxios from "moxios";
+import sinon from "sinon";
+import axios from "axios";
+import {equal} from "assert";
 
 describe('Client', () => {
 	it('should mount Client', async () => {
@@ -12,4 +16,40 @@ describe('Client', () => {
 		})
 		expect(wrapper.exists()).toBe(true);
 	});
+	
+	beforeEach(function () {
+		moxios.install()
+	})
+	
+	afterEach(function () {
+		moxios.uninstall()
+	})
+	
+	test('testing for the post', async()=> {
+		moxios.withMock( ()=> {
+			let wrapper = shallowMount(UpdateTodo,{
+				global: {
+					mocks: {
+						$route: {params: { id: 1}},
+					}
+				}
+			})
+			wrapper = sinon.spy()
+			axios.get('https://jsonplaceholder.typicode.com/posts/id').then(wrapper)
+			
+			moxios.wait(()=> {
+				let request = moxios.requests.mostRecent()
+				request.respondWith({
+						status: 200,
+						response: {
+							title: 'title1', body: 'body1'
+						}
+					})
+					.then(()=> {
+						equal(wrapper.called, true)
+						
+					})
+			})
+		})
+	})
 });
